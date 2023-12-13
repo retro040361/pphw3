cad4090dae27c0bad10ae762b717877e6b277db4
retro040361 <danny5388@gmail.com>
2023-12-13 08:22:07 +0000

    1213

--- hw3-2.cu
+++ hw3-2.cu
@@ -5,14 +5,120 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <cuda.h>
 
-#define INF 1073741823
+#define INF 99999
 
-// const int bs = 512;
-#define bs 64
+// static int bs = 64;
+#define BLOCK_DIM 16
+#define bs 16
+#define BLOCK_SIZE 64
+#define HALF_BLOCK_FACTOR 64
 // const int num_threads = 16; // 調整為您需要的數量
+__forceinline__
+__device__ void block_calc(int* C, int* A, int* B, int bi, int bj) {
+  for (int k = 0; k < bs; k++) {
+    int sum = A[bi*bs + k] + B[k*bs + bj];
+    if (C[bi*bs + bj] > sum) {
+      C[bi*bs + bj] = sum;
+    }
+    __syncthreads();
+  }
+  
+}
+
+__global__ void floyd_warshall_block_kernel_phase1(int n, int k, int* graph) {
+  const unsigned int bi = threadIdx.y;
+  const unsigned int bj = threadIdx.x;
+
+  __shared__ int C[BLOCK_DIM * BLOCK_DIM];
+
+  __syncthreads();
+
+  // Transfer to temp shared arrays
+  C[bi*BLOCK_DIM + bj] = graph[k*BLOCK_DIM*n + k*BLOCK_DIM + bi*n + bj];
+
+  __syncthreads();
+  
+  block_calc(C, C, C, bi, bj);
+
+  __syncthreads();
+
+  // Transfer back to graph
+  graph[k*BLOCK_DIM*n + k*BLOCK_DIM + bi*n + bj] = C[bi*BLOCK_DIM + bj];
+
+}
 
 
+__global__ void floyd_warshall_block_kernel_phase2(int n, int k, int* graph) {
+  // BlockDim is one dimensional (Straight along diagonal)
+  // Blocks themselves are two dimensional
+  const unsigned int i = blockIdx.x;
+  const unsigned int bi = threadIdx.y;
+  const unsigned int bj = threadIdx.x;
+
+  if (i == k) return;
+
+  __shared__ int A[BLOCK_DIM * BLOCK_DIM];
+  __shared__ int B[BLOCK_DIM * BLOCK_DIM];
+  __shared__ int C[BLOCK_DIM * BLOCK_DIM];
+
+  __syncthreads();
+
+  C[bi*BLOCK_DIM + bj] = graph[i*BLOCK_DIM*n + k*BLOCK_DIM + bi*n + bj];
+  B[bi*BLOCK_DIM + bj] = graph[k*BLOCK_DIM*n + k*BLOCK_DIM + bi*n + bj];
+
+  __syncthreads();
+
+  block_calc(C, C, B, bi, bj);
+
+  __syncthreads();
+
+  graph[i*BLOCK_DIM*n + k*BLOCK_DIM + bi*n + bj] = C[bi*BLOCK_DIM + bj];
+
+  // Phase 2 1/2
+
+  C[bi*BLOCK_DIM + bj] = graph[k*BLOCK_DIM*n + i*BLOCK_DIM + bi*n + bj];
+  A[bi*BLOCK_DIM + bj] = graph[k*BLOCK_DIM*n + k*BLOCK_DIM + bi*n + bj];
+
+  __syncthreads();
+
+  block_calc(C, A, C, bi, bj);
+
+  __syncthreads();
+
+  // Block C is the only one that could be changed
+  graph[k*BLOCK_DIM*n + i*BLOCK_DIM + bi*n + bj] = C[bi*BLOCK_DIM + bj];
+}
+
+
+__global__ void floyd_warshall_block_kernel_phase3(int n, int k, int* graph) {
+  // BlockDim is one dimensional (Straight along diagonal)
+  // Blocks themselves are two dimensional
+  const unsigned int j = blockIdx.x;
+  const unsigned int i = blockIdx.y;
+  const unsigned int bi = threadIdx.y;
+  const unsigned int bj = threadIdx.x;
+
+  if (i == k && j == k) return;
+  __shared__ int A[BLOCK_DIM * BLOCK_DIM];
+  __shared__ int B[BLOCK_DIM * BLOCK_DIM];
+  __shared__ int C[BLOCK_DIM * BLOCK_DIM];
+
+  __syncthreads();
+
+  C[bi*BLOCK_DIM + bj] = graph[i*BLOCK_DIM*n + j*BLOCK_DIM + bi*n + bj];
+  A[bi*BLOCK_DIM + bj] = graph[i*BLOCK_DIM*n + k*BLOCK_DIM + bi*n + bj];
+  B[bi*BLOCK_DIM + bj] = graph[k*BLOCK_DIM*n + j*BLOCK_DIM + bi*n + bj];
+
+  __syncthreads();
+
+  block_calc(C, A, B, bi, bj);
+
+  __syncthreads();
+
+  graph[i*BLOCK_DIM*n + j*BLOCK_DIM + bi*n + bj] = C[bi*BLOCK_DIM + bj];
+}
 
 __global__ void block_fw_p1_kernel(int* dist1, int k, int num_vertices)
 {
@@ -23,13 +129,14 @@ __global__ void block_fw_p1_kernel(int* dist1, int k, int num_vertices)
     int y_shift = k*bs + threadIdx.y * num_vertices + threadIdx.x;  //
     shared_block[threadIdx.y*bs + threadIdx.x] = dist1[x_shift*num_vertices + y_shift];
     __syncthreads();
-    int tmp;
-    for(int k=0;k<bs;k++){
-        tmp = shared_block[threadIdx.y*num_vertices + k] + shared_block[k*num_vertices + threadIdx.x];  
-        if(tmp < shared_block[threadIdx.y*num_vertices + threadIdx.x])
-            shared_block[threadIdx.y*num_vertices + threadIdx.x] = tmp;
-        __syncthreads();
-    }
+    block_calc(shared_block,shared_block,shared_block,threadIdx.y,threadIdx.x);
+    // int tmp;
+    // for(int kk=0;kk<bs;kk++){
+    //     tmp = shared_block[threadIdx.x*bs + kk] + shared_block[kk*bs + threadIdx.y];  
+    //     if(tmp < shared_block[threadIdx.x*bs + threadIdx.y])
+    //         shared_block[threadIdx.x*bs + threadIdx.y] = tmp;
+    //     __syncthreads();
+    // }
     __syncthreads();
     dist1[x_shift*num_vertices + y_shift] = shared_block[threadIdx.y*bs + threadIdx.x];
 }
@@ -47,48 +154,50 @@ __global__ void block_fw_p2_kernel(int* dist1, int k, int num_vertices)
     int x_shift_ik = blockIdx.x * bs * num_vertices;
     // int y_shift_ik = k * bs + threadIdx.y * num_vertices + threadIdx.x;  
     
-    shared_block_kk[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_kk + y_shift];
-    shared_block_ik[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_ik + y_shift];
+    shared_block_kk[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_kk + y_shift];    // B
+    shared_block_ik[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_ik + y_shift];    // C
     
     __syncthreads();
-    int tmp;
-    for(int k=0;k<bs;k++){
-        tmp = shared_block_ik[threadIdx.y*num_vertices + k] + shared_block_kk[k*num_vertices + threadIdx.x];  
-        if(tmp < shared_block_ik[threadIdx.y*num_vertices + threadIdx.x])
-            shared_block_ik[threadIdx.y*num_vertices + threadIdx.x] = tmp;
-        __syncthreads();
-    }
+    block_calc(shared_block_ik,shared_block_ik,shared_block_kk,threadIdx.y,threadIdx.x);
+    // int tmp;
+    // for(int kk=0;kk<bs;kk++){
+    //     tmp = shared_block_ik[threadIdx.y*bs + kk] + shared_block_kk[kk*bs + threadIdx.x];  
+    //     if(tmp < shared_block_ik[threadIdx.y*bs + threadIdx.x])
+    //         shared_block_ik[threadIdx.y*bs + threadIdx.x] = tmp;
+    //     __syncthreads();
+    // }
     __syncthreads();
     dist1[x_shift_ik + y_shift] = shared_block_ik[threadIdx.y*bs + threadIdx.x];
 
-    __shared__ int shared_block_ki[bs*bs];
+    __shared__ int shared_block_nkk[bs*bs];  // 
     // int x_shift = k * bs * num_vertices;
     int y_shift_ki = blockIdx.x * bs + threadIdx.y * num_vertices + threadIdx.x;  
     int y_shift_kk = k * bs + threadIdx.y * num_vertices + threadIdx.x;
     // int y_shift_ik = k * bs + threadIdx.y * num_vertices + threadIdx.x;  
     
-    shared_block_kk[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_kk + y_shift_kk];
-    shared_block_ki[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_kk + y_shift_ki];
+    shared_block_nkk[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_kk + y_shift_kk];    // A
+    shared_block_ik[threadIdx.y*bs + threadIdx.x] = dist1[x_shift_kk + y_shift_ki];     // C
     
     __syncthreads();
     // int tmp;
-    for(int k=0;k<bs;k++){
-        tmp = shared_block_kk[threadIdx.y*num_vertices + k] + shared_block_ki[k*num_vertices + threadIdx.x];  
-        if(tmp < shared_block_ki[threadIdx.y*num_vertices + threadIdx.x])
-            shared_block_ki[threadIdx.y*num_vertices + threadIdx.x] = tmp;
-        __syncthreads();
-    }
+    block_calc(shared_block_ik,shared_block_nkk,shared_block_ik,threadIdx.y,threadIdx.x);
+    // for(int kk=0;kk<bs;kk++){
+    //     tmp = shared_block_nkk[threadIdx.y*bs + kk] + shared_block_ik[kk*bs + threadIdx.x];  
+    //     if(tmp < shared_block_ik[threadIdx.y*bs + threadIdx.x])
+    //         shared_block_ik[threadIdx.y*bs + threadIdx.x] = tmp;
+    //     __syncthreads();
+    // }
     __syncthreads();
-    dist1[x_shift_kk + y_shift_ki] = shared_block_ki[threadIdx.y*bs + threadIdx.x];
+    dist1[x_shift_kk + y_shift_ki] = shared_block_ik[threadIdx.y*bs + threadIdx.x];
 
 }
 
 __global__ void block_fw_p3_kernel(int* dist1, int k, int num_vertices){
-    if(blockIdx.x==k||blockIdx.y==k)
+    if(blockIdx.x==k&&blockIdx.y==k)
         return;
-    __shared__ int shared_block_ij[bs*bs];
-    __shared__ int shared_block_ik[bs*bs];
-    __shared__ int shared_block_kj[bs*bs];
+    __shared__ int shared_block_ij[bs*bs];  // C
+    __shared__ int shared_block_ik[bs*bs];  // A
+    __shared__ int shared_block_kj[bs*bs];  // B
     __syncthreads();
     int x_shift_i = blockIdx.y * bs * num_vertices;
     int x_shift_k = k * bs * num_vertices;
@@ -99,13 +208,14 @@ __global__ void block_fw_p3_kernel(int* dist1, int k, int num_vertices){
     shared_block_kj[threadIdx.y * bs + threadIdx.x] = dist1[x_shift_k + y_shift_j];
 
     __syncthreads();
-    int tmp;
-    for(int k=0;k<bs;k++){
-        tmp = shared_block_ik[threadIdx.y*num_vertices + k] + shared_block_kj[k*num_vertices + threadIdx.x];  
-        if(tmp < shared_block_ij[threadIdx.y*num_vertices + threadIdx.x])
-            shared_block_ij[threadIdx.y*num_vertices + threadIdx.x] = tmp;
-        __syncthreads();
-    }
+    block_calc(shared_block_ij,shared_block_ik,shared_block_kj,threadIdx.y,threadIdx.x);
+    // int tmp;
+    // for(int kk=0;kk<bs;kk++){
+    //     tmp = shared_block_ik[threadIdx.y*bs + kk] + shared_block_kj[kk*bs + threadIdx.x];  
+    //     if(tmp < shared_block_ij[threadIdx.y*bs + threadIdx.x])
+    //         shared_block_ij[threadIdx.y*bs + threadIdx.x] = tmp;
+    //     __syncthreads();
+    // }
     __syncthreads();
     dist1[x_shift_i + y_shift_j] = shared_block_ij[threadIdx.y * bs + threadIdx.x];
 }
@@ -116,33 +226,36 @@ int* readBinaryFile(char* filename, int& vertices, int& edges, int& pad_vertices
         perror("Error opening file");
         exit(1);
     }
-    
-    fread(&vertices, sizeof(int), 1, file);
-    fread(&edges, sizeof(int), 1, file);
+    size_t elements_read;
+    elements_read = fread(&vertices, sizeof(int), 1, file);
+    elements_read = fread(&edges, sizeof(int), 1, file);
     
     // int* dist = (int**)malloc(vertices * sizeof(int*));
+    // if(vertices < bs)
+        // bs = vertices;
     if(vertices%bs)
-        pad_vertices = vertices + bs + vertices%bs;
+        pad_vertices = vertices + bs - (vertices%bs);
     else
         pad_vertices = vertices;
     int* dist = new int[pad_vertices*pad_vertices];
     for (int i = 0; i < pad_vertices; ++i) {
         // dist[i] = (int*)malloc(vertices * sizeof(int));
         for (int j = 0; j < pad_vertices; ++j) {
-            if(i==j&&i<vertices&&j<vertices)
-                dist[i*vertices + j] = 0;
+            if(i==j&&i<pad_vertices&&j<pad_vertices)
+                dist[i*pad_vertices + j] = 0;
             else
-                dist[i*vertices + j] = INF;
+                dist[i*pad_vertices + j] = INF;
         }
     }
     int src, dst, weight;
+    
     for (int i = 0; i < edges; i++) {
-        fread(&src, sizeof(int), 1, file);
-        fread(&dst, sizeof(int), 1, file);
-        fread(&weight, sizeof(int), 1, file);
+        elements_read = fread(&src, sizeof(int), 1, file);
+        elements_read = fread(&dst, sizeof(int), 1, file);
+        elements_read = fread(&weight, sizeof(int), 1, file);
 
         // Assuming vertices are 0-indexed
-        dist[src*vertices+dst] = weight;
+        dist[src*pad_vertices+dst] = weight;
     }
 
     fclose(file);
@@ -150,21 +263,19 @@ int* readBinaryFile(char* filename, int& vertices, int& edges, int& pad_vertices
 }
 
 
-void writeBinaryFile(char* FileName, int* dist, int n) {
+void writeBinaryFile(char* FileName, int* dist, int n, int pad_n) {
     FILE* outfile = fopen(FileName, "wb");
-    // #pragma omp parallel for num_threads(num_threads)  schedule(dynamic, 1) 
+ 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (dist[i*n+j] >= INF) 
-                dist[i*n+j] = INF;
+            if (dist[i*pad_n+j] >= INF) 
+                dist[i*pad_n+j] = INF;
         }
-        fwrite(dist+i*n, sizeof(int), n, outfile); 
+        fwrite(dist+i*pad_n, sizeof(int), n, outfile); 
     }
-    // std::cout<<"hi"<<std::endl;
-    // fwrite(dist, sizeof(int), n*n, outfile);
-    // std::cout<<"write done"<<std::endl;
+
     fclose(outfile);
-    // std::cout<<"fclose"<<std::endl;
+
 }
 
 int main(int argc, char* argv[]) {
@@ -177,21 +288,28 @@ int main(int argc, char* argv[]) {
     cudaMalloc((void**)&d_dist, pad_vertices * pad_vertices * sizeof(int));
     cudaMemcpy(d_dist, dist, pad_vertices * pad_vertices * sizeof(int), cudaMemcpyHostToDevice);
     
-    int round = std::ceil((double)num_vertices / bs);
+    int round = pad_vertices/bs; //std::ceil((double)num_vertices / bs);
     dim3 blockSize(bs, bs, 1);
     dim3 gridSize( round, round, 1);
+    
+    const int blocks = (pad_vertices + bs - 1) / bs;
+    // dim3 block_dim(bs, bs, 1);
+    dim3 phase4_grid(blocks, blocks, 1);
 
-
-    for (int k = 0; k < round; k++) {
+    for (int k = 0; k < blocks; k++) {
         block_fw_p1_kernel<<<1, blockSize>>>(d_dist, k, pad_vertices);
         block_fw_p2_kernel<<<round, blockSize>>>(d_dist, k, pad_vertices);
         block_fw_p3_kernel<<<gridSize, blockSize>>>(d_dist, k, pad_vertices);
+        // floyd_warshall_block_kernel_phase1<<<1, blockSize>>>(pad_vertices, k, d_dist);
+        // floyd_warshall_block_kernel_phase2<<<blocks, blockSize>>>(pad_vertices, k, d_dist);
+        // floyd_warshall_block_kernel_phase3<<<phase4_grid, blockSize>>>(pad_vertices, k, d_dist);
+
     }
 
     cudaMemcpy(dist, d_dist, pad_vertices * pad_vertices * sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(d_dist);
 
-
-    writeBinaryFile(output_file, dist, num_vertices);
+    // std::cout<<pad_vertices<<" "<<num_vertices<<std::endl;
+    writeBinaryFile(output_file, dist, num_vertices, pad_vertices);
     return 0;
 }
